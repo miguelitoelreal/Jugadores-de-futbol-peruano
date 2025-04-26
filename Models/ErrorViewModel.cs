abbada9034e5282290362ad5fc3190a4344463c9
miguelitoelreal <miguegara20@gmail.com>
2025-04-25 22:36:35 -0500

    Primer commit - Proyecto base creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JugadoresFutbolPeruano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
