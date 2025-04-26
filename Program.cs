21676e9bf95356c3d3884cdd7eda65391772365d
miguelitoelreal <miguegara20@gmail.com>
2025-04-26 03:14:17 -0500

    version2.1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configurar la conexión a PostgreSQL desde una variable de entorno
 var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Transformar DATABASE_URL al formato compatible con Npgsql
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString = $"Host={uri.Host};Port={uri.Port};Username={userInfo[0]};Password={userInfo[1]};Database={uri.AbsolutePath.TrimStart('/')};SSL Mode=Require;Trust Server Certificate=true";
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
