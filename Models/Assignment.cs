e107d687a93c8a8ac3b84afd217233e5ca496b55
miguelitoelreal <miguegara20@gmail.com>
2025-04-25 23:20:38 -0500

    Rama: feature/modelo-datos

---
+++ Models/Assignment.cs
@@ -0,0 +1,13 @@
+namespace JugadoresFutbolPeruano.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,31 @@
+using JugadoresFutbolPeruano.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar la conexión a PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Agregar servicios al contenedor
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de solicitudes HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
